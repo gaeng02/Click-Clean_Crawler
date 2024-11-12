422a117f2ad08c232fc67fc538352d5028203d20
gaeng02 <sondin144@gmail.com>
2024-11-12 23:33:04 +0900

    sample

---
+++ sample/test.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://n.news.naver.com/article/448/0000488553?sid=101"
+    
+headers = {"User-Agent" : "Chrome"}
+    
+response = requests.get(url, headers = headers)
+
+if (response.status_code == 200) :
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    title = soup.find("h2", id = "title_area").get_text(strip = True)
+    content = soup.find("article", id = "dic_area").get_text(strip = True)
+    raw_content = soup.find("article", {"class" : "go_trans _article_content"})
+
+    with open ("test.txt", "w") as f :
+        f.write(title + "\n\n")
+        f.write(content + "\n\n")
+        f.write(str(raw_content))
+        
+else : print(response.status_code)
