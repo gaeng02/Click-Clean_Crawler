6f1e6a79c1eddf30101c3dfad5d3daa554ada8ef
gaeng02 <sondin144@gmail.com>
2024-11-17 21:39:02 +0900

    update

--- src/naver.py
+++ src/naver.py
@@ -1,29 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
+from packaging import make_json
 
-def naver (num) :
+def naver (media, field) :
 
-    url = "https://media.naver.com/press/" + media_field[num][0] + "?sid=" + media_field[num][1]
-    
+    # url = "https://media.naver.com/press/" + media + "?sid=" + field
+
     headers = {"User-Agent" : "Chrome"}
     
     response = requests.get(url, headers = headers)
 
-    if (response.status_code == 200) :
-        soup = BeautifulSoup(responce.text, "html.parser")
-        print(soup)
-        
-    else : print(response.status_code)
+    if (response.status_code != 200) :
+        return response.status_code
     
-    # news_content = soup.find("article", id = "dic_area")
-    # print(news_content)
+    soup = BeautifulSoup(response.text, "html.parser")
 
+    title = soup.find("h2", id = "title_area").text
+    raw_contents = soup.find("article", id = "dic_area")
+    # url
+    reporter = re.sub(r"\s*기자\s*", "", soup.find("em", class_ = "media_end_head_journalist_name").text)
+    # media
+    # date
+    # tag
+    
 
 if (__name__ == "__main__") :
     
     # media = 000  - 언론사 : 001 ~ ... 
     # field = 100  - 분야 : 100 ~ ...
     
-    media_field = [["000", "100"], ["000", "101"]]
-    naver(0)
+    naver(0, 0)
