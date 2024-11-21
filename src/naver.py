723775c1f1f99563fd483e96c2a8854ea994fc40
gaeng02 <sondin144@gmail.com>
2024-11-21 21:39:48 +0900

    changed tag for FE

--- src/naver.py
+++ src/naver.py
@@ -18,31 +18,52 @@ def naver (media, field) :
     soup = BeautifulSoup(response.text, "html.parser")
 
 
-    # title
+    ''' title '''
     title = soup.find("h2", id = "title_area").text
 
-    # raw_contents
+    ''' body '''
     raw_contents_with_tag = soup.find("article", id = "dic_area")
-    raw_contents = str(raw_contents_with_tag)
-    
-    # url
 
-    # reporter
+    # data-src -> src
+    for img in soup.find_all("img") :
+        if 'data-src' in img.attrs :  
+            img['src'] = img['data-src']  
+            del img['data-src']
+
+    # delete style = \"display: none;\
+    for tag in soup.find_all(style = True) :  
+        if 'display: none;' in tag['style'] :
+            tag['style'] = tag['style'].replace('display: none;', '').strip()
+            if not tag['style'] : del tag['style']
+
+    body = str(raw_contents_with_tag)
+    
+    
+    ''' url '''
+    
+
+    ''' author '''
     reporters = soup.find_all("em", class_="media_end_head_journalist_name")
     reporter = [re.sub(r"\s*기자\s*", "", reporter.text) for reporter in reporters]
     
-    # media
+    author = ", ".join(reporter)
+    
+    
+    ''' media '''
     media = soup.find("span", class_ = "media_end_head_top_logo_text light_type _LAZY_LOADING_ERROR_SHOW").text
+
     
-    # date :: f"YEAR.MONTH.DAY AM/PM TT:MM"
-    date = soup.find("span", class_ = "media_end_head_info_datestamp_time _ARTICLE_DATE_TIME").text
+    ''' created_at '''
+    # f"YEAR.MONTH.DAY AM/PM TT:MM"
+    created_at = soup.find("span", class_ = "media_end_head_info_datestamp_time _ARTICLE_DATE_TIME").text
+
     
-    # tag
+    ''' category ''' 
     active_tag = soup.find("li", class_ = "Nlist_item _LNB_ITEM is_active")
-    tag = active_tag.find("span", class_="Nitem_link_menu").text
+    category = active_tag.find("span", class_="Nitem_link_menu").text
 
 
-    make_json(title, raw_contents, url, reporter, media, date, tag)
+    make_json(title, body, url, author, media, created_at, category)
     
 
 if (__name__ == "__main__") :
